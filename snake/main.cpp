19f644c4556a5503b8feca3827cac56f2d0d6d02
Benjamin Warner <benjaminnwarner@gmail.com>
2015-12-05 00:50:21 -0500

    Initial Commit

---
+++ snake/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <sdl.h>
+
+int main(int argc, char* argv[]){
+	
+	
+	return 0;
+}
---
+++ snake/snake.h
@@ -0,0 +1,26 @@
+#ifndef SNK_SNAKE_H
+#define SNK_SNAKE_H
+
+#include <iostream>
+#include <vector>
+
+class snake{
+public:
+	snake();
+	~snake();
+	void addSegment();
+	void updateSnake();
+	void drawSnake();
+
+private:
+	std::vector<char> segments;
+};
+
+snake::snake(){
+	segments.push_back('O');
+	segments.push_back('o');
+}
+
+snake::~snake(){}
+
+#endif
