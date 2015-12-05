653a366f2e1555aaee887a8a3585c23cb088c78c
matt-piechuta <piechuta2111@yahoo.com>
2015-12-05 01:32:10 -0500

    created game board

--- snake/level.h
+++ snake/level.h
@@ -2,17 +2,48 @@
 #define SNK_LEVEL_H
 
 #include <iostream>
-#include "snake.h"
-
 
 class box{
 public:
 	box();
 	~box();
-	
+	void setSize(int);
+	void initialize();
+	void deployFruit();
+	void startSnake();
+	void handleInput();
 
 private:
-	snake player;
+	char ** level;
+	int field_size;
 };
+
+box::box(){
+
+}
+
+box::~box(){
+
+}
+
+void box::setSize(int size){
+	field_size = size;
+}
+
+void box::initialize(){
+	level = new char*[field_size];
+	for (int i = 0; i < field_size; ++i){
+		level[i] = new char[field_size];
+	}
+
+	for (int i = 0; i < field_size; ++i){
+		for (int j = 0; j < field_size; ++j){
+			if (i == 0 || i == field_size || j == 0 || j == field_size)
+				level[i][j] = '+';
+			else
+				level[i][j] = ' ';
+		}
+	}
+}
 
 #endif
